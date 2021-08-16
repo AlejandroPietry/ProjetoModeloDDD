314943abf919f28feed925a41621b72084900658
Alejandro Pietry <pietrytrindade12@gmail.com>
2021-08-16 00:14:37 -0300

    first commit

---
+++ ProjetoModeloDDD.Infra.Data/Context/ProjetoModeloContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoModeloDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Infra.Data.Context
+{
+    public class ProjetoModeloContext : DbContext
+    {
+        public ProjetoModeloContext(DbContextOptions<ProjetoModeloContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
