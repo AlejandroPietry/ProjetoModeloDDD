7f10a6c69fd70e0d2ee552cfb28048aee2dabab2
Alejandro Pietry <pietrytrindade12@gmail.com>
2021-08-22 22:29:36 -0300

    fazendo o create para o cadastro de pedidos

--- ProjetoModeloDDD.MVC/Controllers/PedidosController.cs
+++ ProjetoModeloDDD.MVC/Controllers/PedidosController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 
 namespace ProjetoModeloDDD.MVC.Controllers
 {
+    [Route("pedidos")]
     public class PedidosController : Controller
     {
         private readonly IPedidoAppService _pedidoAppService;
@@ -16,14 +17,28 @@ namespace ProjetoModeloDDD.MVC.Controllers
             _pedidoAppService = pedidoAppService;
             _mapper = mapper;
         }
+
         public IActionResult Index()     
         {
-            var a = _pedidoAppService.GetPedidosFromIndex();
-
             IEnumerable<PedidoViewModel> listaPedidos =
                 _mapper.Map<IEnumerable<Pedido>, IEnumerable<PedidoViewModel>>(_pedidoAppService.GetPedidosFromIndex());
 
             return View(listaPedidos);
+        }
+        [HttpGet, Route("criar")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("criar")]
+        public IActionResult Create(PedidoViewModel pedidoViewModel)
+        {
+            var pedidoDomain = _mapper.Map<PedidoViewModel, Pedido>(pedidoViewModel);
+            _pedidoAppService.Add(pedidoDomain);
+            TempData["alertaSucessoNoEnvio"] = "Salvo com sucesso";
+            return RedirectToAction("Index");
+
         }
     }
 }
